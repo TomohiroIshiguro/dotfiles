7a1ce9a976ec7b2b6bfd96c7b26422b5aff893ad
Tomohiro Ishiguro <se.ishiguro.tomohiro@gmail.com>
2023-01-25 10:08:49 +0900

    MOD add apps.

--- apps/install-in-mac.sh
+++ apps/install-in-mac.sh
@@ -5,6 +5,9 @@ set -euo pipefail
 
 # for utilities
 brew install karabiner-elements #CASK
+brew install oogle-drive-file-stream #CASK
+brew install sublime-text #CASK
+brew install xmind #CASK
 brew install clipy #CASK
 brew install flux #CASK
 brew install tmux
@@ -12,7 +15,12 @@ brew install tmux
 # for development
 brew install visual-studio-code #CASK
 brew install sequel-ace #CASK
+brew install docker #CASK
+brew install cyberduck #CASK
 
 # others
 brew install google-chrome #CASK
+brew install brave-browser #CASK
 brew install evernote #CASK
+brew install biscuit #CASK
+brew install kindle #CASK
