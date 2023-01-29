2dea8e828425b60610ce22607af01dbb241e0a27
Tomohiro Ishiguro <se.ishiguro.tomohiro@gmail.com>
2023-01-29 09:57:39 +0900

    MOD install app script.

--- apps/install-in-mac.sh
+++ apps/install-in-mac.sh
@@ -4,15 +4,21 @@ set -euo pipefail
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # for utilities
+brew install google-japanese-ime
+brew install tmux
 brew install karabiner-elements #CASK
-brew install oogle-drive-file-stream #CASK
+brew install google-drive #CASK
 brew install sublime-text #CASK
 brew install xmind #CASK
 brew install clipy #CASK
 brew install flux #CASK
-brew install tmux
 
 # for development
+brew install git
+brew install awscli
+brew install tfenv && \
+  tfenv install latest && \
+  tfenv use latest
 brew install visual-studio-code #CASK
 brew install sequel-ace #CASK
 brew install docker #CASK
@@ -24,3 +30,6 @@ brew install brave-browser #CASK
 brew install evernote #CASK
 brew install biscuit #CASK
 brew install kindle #CASK
+brew install slack #CASK
+
+brew list
